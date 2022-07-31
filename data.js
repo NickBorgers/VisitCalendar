91a877f3c3fed077dd5fbec9506f2a787f507d5f
Nick Borgers <nickborgers@gmail.com>
2022-07-31 11:48:58 -0400

    change label of calendar events

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 var readable_data = [
   { eventName: 'Settling in', calendar: 'Life', color: 'orange', start_date: moment("2022-06-14"), end_date: moment("2022-08-11")},
-  { eventName: 'Visitors', calendar: 'Fun', color: 'blue', start_date: moment("2022-10-06"), end_date: moment("2022-10-12")},
-  { eventName: 'Visitors', calendar: 'Fun', color: 'blue', start_date: moment("2022-09-09"), end_date: moment("2022-09-12")},
+  { eventName: 'Busy', calendar: 'Fun', color: 'blue', start_date: moment("2022-10-06"), end_date: moment("2022-10-12")},
+  { eventName: 'Busy', calendar: 'Fun', color: 'blue', start_date: moment("2022-09-09"), end_date: moment("2022-09-12")},
 ]
